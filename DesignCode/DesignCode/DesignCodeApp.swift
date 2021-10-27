416bb6ebcd3a31fb6ef7bb70462a166d85383578
stefidka <stef.vacheva@gmail.com>
2021-10-27 18:56:46 +0300

    project init

---
+++ DesignCode/DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Stefka Vacheva on 27.10.21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
